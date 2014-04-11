6222a0a2b63fa6dcfdd3eca7be7350bf48e83e7d
jtremesaygues <jonathan.tremesaygues@gmail.com>
2014-04-11 19:17:02 +0200

    migrate form-inout to new render system

--- snippets/form-inout.php
+++ snippets/form-inout.php
@@ -1,53 +1,56 @@
 <?php
 
-$vars['dynamic_form'] = json_encode(array(
-    array(
-        'tag' => 'input',
-        'type' => 'text',
-        'name' => 'login',
-        'id' => 'form-login',
-        'label' => 'Login',
-        'placeholder' => 'Enter your login'
-    ),
-    array(
-        'tag' => 'input',
-        'type' => 'email',
-        'name' => 'email',
-        'id' => 'form-email',
-        'label' => 'Email',
-        'placeholder' => 'Enter your email'
-    ),
-    array(
-        'tag' => 'textarea',
-        'name' => 'biography',
-        'id' => "form-biography",
-        'label' => 'Biography',
-        'placeholder' => 'Enter your biography'
-    ),
-    array(
-        'tag' => 'input',
-        'type' => 'number',
-        'name' => 'favorites_numbers[]',
-        'id' => 'form-favorite-number1',
-        'label' => 'Favorite number 1',
-        'placeholder' => 'Enter your favorite number'
-    ),
-    array(
-        'tag' => 'input',
-        'type' => 'number',
-        'name' => 'favorites_numbers[]',
-        'id' => 'form-favorite-number2',
-        'label' => 'Favorite number 2',
-        'placeholder' => 'Enter your second favorite number'
-    ),
-    array(
-        'tag' => 'input',
-        'type' => 'number',
-        'name' => 'favorites_numbers[]',
-        'id' => 'form-favorite-number3',
-        'label' => 'Favorite number 3',
-        'placeholder' => 'Enter your third favorite number'
-    ),
-));
+function page_snippet_callback(&$template_vars)
+{
+    $template_vars['dynamic_form'] = json_encode(array(
+        array(
+            'tag' => 'input',
+            'type' => 'text',
+            'name' => 'login',
+            'id' => 'form-login',
+            'label' => 'Login',
+            'placeholder' => 'Enter your login'
+        ),
+        array(
+            'tag' => 'input',
+            'type' => 'email',
+            'name' => 'email',
+            'id' => 'form-email',
+            'label' => 'Email',
+            'placeholder' => 'Enter your email'
+        ),
+        array(
+            'tag' => 'textarea',
+            'name' => 'biography',
+            'id' => "form-biography",
+            'label' => 'Biography',
+            'placeholder' => 'Enter your biography'
+        ),
+        array(
+            'tag' => 'input',
+            'type' => 'number',
+            'name' => 'favorites_numbers[]',
+            'id' => 'form-favorite-number1',
+            'label' => 'Favorite number 1',
+            'placeholder' => 'Enter your favorite number'
+        ),
+        array(
+            'tag' => 'input',
+            'type' => 'number',
+            'name' => 'favorites_numbers[]',
+            'id' => 'form-favorite-number2',
+            'label' => 'Favorite number 2',
+            'placeholder' => 'Enter your second favorite number'
+        ),
+        array(
+            'tag' => 'input',
+            'type' => 'number',
+            'name' => 'favorites_numbers[]',
+            'id' => 'form-favorite-number3',
+            'label' => 'Favorite number 3',
+            'placeholder' => 'Enter your third favorite number'
+        ),
+    ));
 
-$vars['loop_count'] = range(0, 5);
+    $template_vars['loop_count'] = range(0, 5);
+}
