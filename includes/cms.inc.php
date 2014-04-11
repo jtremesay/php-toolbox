d0e12a0abc40d30d8170650b6bce3461c532f5fa
jtremesaygues <jonathan.tremesaygues@gmail.com>
2014-04-11 11:16:15 +0200

    seksy cms #oupas

--- includes/cms.inc.php
+++ includes/cms.inc.php
@@ -1,29 +1,79 @@
 <?php
 
-// Navigation helpers
-function findPageById($id)
+class CMS
 {
-    global $pages;
+    protected $navigation;
 
-    $page = _findPageById($id, $pages);
-
-    return $page;
-}
-
-function _findPageById($id, $pages)
-{
-    foreach ($pages as $element) {
-        if ($element['type'] === 'page' && $element['id'] === $id) {
-            return $element;
-        }
-
-        if ($element['type'] === 'dropdown') {
-            $page = _findPageById($id, $element['pages']);
-            if (!is_null($page)) {
-                return $page;
-            }
-        }
+    function __construct($navigation)
+    {
+        $this->navigation = $navigation;
     }
 
-    return null;
+    public function renderAskedPage()
+    {
+        $page_id = isset($_GET['id']) ? $_GET['id'] : '';
+        $render = $this->renderPage($page_id);
+
+        return $render;
+    }
+
+
+    public function renderPage($page_id)
+    {
+        $page = $this->findPageById($page_id);
+        if (is_null($page)) {
+            $page = array(
+                'type' => 'page',
+                'id' => 'index',
+                'title' => 'Main',
+            );
+        }
+
+        $vars = array();
+        $vars['pages'] = $this->navigation;
+        $vars['current_page'] = $page;
+
+
+        $snippet_path = 'snippets/'.$page['id'].'.php';
+        if (file_exists($snippet_path)) {
+            require $snippet_path;
+        }
+
+        $render = '';
+        $template_name = $page['type'].'-'.$page['id'];
+        $template_path = 'templates/'.$template_name.'.html';
+        if (file_exists($template_path)) {
+            $tpl = new Rain\Tpl();
+            $tpl->assign($vars);
+            $tpl->draw($template_name);
+        }
+
+        return $render;
+    }
+
+
+    protected function findPageById($id)
+    {
+        $page = $this->findPageByIdAndNavigation($id, $this->navigation);
+
+        return $page;
+    }
+
+    protected function findPageByIdAndNavigation($id, $navigation)
+    {
+        foreach ($navigation as $element) {
+            if ($element['type'] === 'page' && $element['id'] === $id) {
+                return $element;
+            }
+
+            if ($element['type'] === 'dropdown') {
+                $page = $this->findPageByIdAndNavigation($id, $element['pages']);
+                if (!is_null($page)) {
+                    return $page;
+                }
+            }
+        }
+
+        return null;
+    }
 }
