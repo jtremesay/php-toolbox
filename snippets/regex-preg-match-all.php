0e9c3a7ab4899a45e3479689aa52a91d18dbd64d
jtremesaygues <jonathan.tremesaygues@gmail.com>
2014-04-11 19:13:59 +0200

    cleanup

--- snippets/regex-preg-match-all.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function page_snippet_callback(&$template_vars)
-{
-    $template_vars['preg_flag_offset_capture'] = PREG_OFFSET_CAPTURE;
-    $template_vars['preg_flag_set_order'] = PREG_SET_ORDER;
-    $template_vars['preg_flag_pattern_order'] = PREG_PATTERN_ORDER;
-}
--- snippets/regex-preg-match.php
+++
@@ -1,3 +0,0 @@
-<?php
-
-$vars['preg_flag_offset_capture'] = PREG_OFFSET_CAPTURE;
